1985f4097b7dee29e79c98c3aa997a297756d9a5
Ricardo Calvo Mendez <rcalvome@purdue.edu>
2025-10-30 18:00:50 -0400

    Multiple execution and logging

--- src/logger.py
+++ src/logger.py
@@ -1,24 +1,27 @@
 """  Logger configuration """
 
 # System
-from datetime import datetime
-import logging
 from typing import Optional
+import logging
+import os
+
 
 def init_logging(log_file: Optional[str]):
     """Creates the basic configuration"""
     handlers = []
-    
+
     if log_file:
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
         handlers.append(logging.FileHandler(log_file))
     else:
         handlers.append(logging.StreamHandler())
-        
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] (%(filename)s:%(lineno)d) %(message)s",
         handlers=handlers
     )
+
 
 def setup_logger(name: str):
     """ Configures a new logger"""
